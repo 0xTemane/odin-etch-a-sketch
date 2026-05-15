64ad9f04d3886d8e06925ad6bb8a523770655b6f
0xTemane <170125706+0xTemane@users.noreply.github.com>
2026-05-15 16:58:50 +0200

    finish createGrid() and fix buttonGridSize

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 const gridContainer = document.querySelector(".grid-container");
 const buttonGridSize = document.querySelector("#btn-grid-size");
+buttonGridSize.addEventListener("click", () => {
+    const input = prompt("Enter a number between 1 and 100 😉");
+    if (input >= 1 && input <= 100){
+        createGrid(input);
+    } else {
+        alert("Sorry, your input is incorrect 😔");
+    }
+})
 
 
 function createGrid(gridSize = 16) {
     gridSize = Math.round(gridSize);
-    for (let i = 0; i < (gridSize * gridSize); i++){
+    while(gridContainer.lastChild){
+        gridContainer.removeChild(gridContainer.lastChild);
+    } for (let i = 0; i < (gridSize * gridSize); i++){
         const square = document.createElement("div");
         square.classList.add("square");
         square.style.flexBasis = `${800 / gridSize}px`;
