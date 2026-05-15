801bf7b328fc30185ef1054a19daaabf66831b8c
0xTemane <170125706+0xTemane@users.noreply.github.com>
2026-05-15 11:41:39 +0200

    Fix broken grid

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const gridContainer = document.querySelector(".grid-container");
 
-for (let i = 0; i <= (16*16); i++){
+for (let i = 0; i < (16*16); i++){
     const square = document.createElement("div");
     square.classList.add("square");
+    square.style.flexBasis = `${800 / 16}px`;
     gridContainer.appendChild(square);
 }
