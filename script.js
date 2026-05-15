57ea9d8d0c55b20bdebf6b9b852e19c831051d77
0xTemane <170125706+0xTemane@users.noreply.github.com>
2026-05-15 17:33:35 +0200

    Create grid reset

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 const gridContainer = document.querySelector(".grid-container");
 const buttonGridSize = document.querySelector("#btn-grid-size");
 const buttonToggleBorder = document.querySelector("#btn-toggle-border");
+const buttonReset = document.querySelector("#btn-reset");
+const actualGridSize = 16;
 
 buttonGridSize.addEventListener("click", () => {
     const input = prompt("Enter a number between 1 and 100 😉");
+    gridSize = input;
     if (input >= 1 && input <= 100){
         createGrid(input);
     } else {
         alert("Sorry, your input is incorrect 😔");
     }
-})
+});
 
 buttonToggleBorder.addEventListener("click", () => {
     const squares = document.querySelectorAll(".square");
     squares.forEach(square => square.classList.toggle("border"))
-})
+});
+
+buttonReset.addEventListener("click", () => createGrid(actualGridSize));
 
 function createGrid(gridSize = 16) {
     gridSize = Math.round(gridSize);
