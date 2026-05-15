22d21ba215bae878d88696d358d14e1a16ce8ff6
0xTemane <170125706+0xTemane@users.noreply.github.com>
2026-05-15 16:43:56 +0200

    Add Grid resize button

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const gridContainer = document.querySelector(".grid-container");
+const ButtonGridSize = document.querySelector("#btn-grid-size");
 
 for (let i = 0; i < (16*16); i++){
     const square = document.createElement("div");
