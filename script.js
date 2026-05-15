da44d0984ca8815529bc2266fd7b833714cd28cf
0xTemane <170125706+0xTemane@users.noreply.github.com>
2026-05-15 19:04:06 +0200

    Finish color buttons logic

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const gridContainer = document.querySelector(".grid-container");
 const buttonGridSize = document.querySelector("#btn-grid-size");
 const buttonToggleBorder = document.querySelector("#btn-toggle-border");
 const buttonReset = document.querySelector("#btn-reset");
+const colorContainer = document.querySelector(".color-container");
+const blackButton = document.querySelector("#btn-black");
+const eraseButton = document.querySelector("#btn-erase");
+const rainbowButton = document.querySelector("#btn-rainbow");
+let selectedColor = "black";
 let actualGridSize = 16;
 
 buttonGridSize.addEventListener("click", () => {
@@ -21,6 +26,29 @@ buttonToggleBorder.addEventListener("click", () => {
 
 buttonReset.addEventListener("click", () => createGrid(actualGridSize));
 
+colorContainer.addEventListener("click", e => {
+    switch(e.target.id){
+        case "btn-black":
+            selectedColor = "black";
+            e.target.style.border = "5px solid rgb(26, 189, 26)";
+            eraseButton.style.border = "none";
+            rainbowButton.style.border = "none";
+            break;
+        case "btn-erase":
+            selectedColor = "erase";
+            e.target.style.border = "5px solid rgb(26, 189, 26)";
+            blackButton.style.border = "none";
+            rainbowButton.style.border = "none";
+            break;
+        case "btn-rainbow":
+            selectedColor = "rainbow";
+            e.target.style.border = "5px solid rgb(26, 189, 26)";
+            eraseButton.style.border = "none";
+            blackButton.style.border = "none";
+            break;
+    }
+})
+
 function createGrid(gridSize = 16) {
     gridSize = Math.round(gridSize);
     while(gridContainer.lastChild){
@@ -30,9 +58,24 @@ function createGrid(gridSize = 16) {
         square.classList.add("square");
         square.classList.add("border");
         square.style.flexBasis = `${800 / gridSize}px`;
-        square.addEventListener("mouseover", () => square.classList.add("black"))
+        square.addEventListener("mouseover", () => {
+            switch(selectedColor){
+                case "black":
+                    square.style.backgroundColor = "black";
+                    break;
+                case "erase":
+                    square.style.backgroundColor = "white";
+                    break;
+                case "rainbow":
+                    square.style.backgroundColor = generateRandomColor();
+            }
+        })
         gridContainer.appendChild(square);
     }
 };
+
+function generateRandomColor(){
+    return `rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`
+}
 
 createGrid();
